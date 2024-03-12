c56c2d7104f78ba084e8852e4a8231c37ec1b8ec
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-12 00:22:12 -0500

    Update declutter.js

--- declutter.js
+++ declutter.js
@@ -1,21 +1,23 @@
 globalThis.defaultResponses = function(request) {
 
-    if (request.url.includes('google9b9ccfe85609fdf9.html')) {
-        return fetch('https://files.servleteer.com/google9b9ccfe85609fdf9.html');
-    }
+  if (request.url.includes('google9b9ccfe85609fdf9.html')) {
+      return fetch('https://files.servleteer.com/google9b9ccfe85609fdf9.html');
+  }
 
-    if (request.url.split('?')[0].split('#')[0].endsWith('robots.txt')) {
-        return new Response(
-            `User-agent: *
-    Allow: /`);
-    }
-    if (`${request.headers.get('user-agent')}`.includes('bot')) {
-        return new Response('Go away bot', {
-            status: 403,
-            statusText: 'Go away bot'
-        });
-    }
-
-    return false;
+  if (request.url.split('?')[0].split('#')[0].endsWith('robots.txt')) {
+      return new Response(
+          `User-agent: *
+  Allow: /`);
+  }
+  if (`${request.headers.get('user-agent')}`.includes('bot')) {
+      return new Response('Go away bot', {
+          status: 403,
+          statusText: 'Go away bot'
+      });
+  }
+  if(request.url.includes('sw.js')){
+    return zfetch(`https://patrick-ring-motive.github.io/FinancialJournal/injects/sw.js?${new Date().getTime()}`);
+  }
+  return false;
 
 }
