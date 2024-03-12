4fe5cb02674fbf2b33dcec915b3ec9eb0143f728
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-11 23:29:46 -0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,60 +1,68 @@
 //utils so as to not bloat the actualy logic of the worker
-globalThis.utilImports=true;
+globalThis.utilImports = true;
 
 globalThis.zfetch = async function() {
     try {
         return await fetch(...arguments);
     } catch (e) {
         console.log(e);
-        return new Response(arguments[0]+'\n'+e.message+'\n'+e.stack, {
+        return new Response(arguments[0] + '\n' + e.message + '\n' + e.stack, {
             status: 569,
             statusText: e.message
         });
     }
-  };
-   
-globalThis.makeHostMap = function(map){
+};
+
+globalThis.makeHostMap = function(map) {
 
     globalThis.hostMap = map;
-    globalThis.hostMap.get=(key)=>{
-        if(globalThis.hostMap[key]){
-          return globalThis.hostMap[key];
+    globalThis.hostMap.get = (key) => {
+        if (globalThis.hostMap[key]) {
+            return globalThis.hostMap[key];
         }
         return globalThis.hostMap.default;
-      }
+    }
 }
 
-String.prototype.toCharCodes=function(){
-    let charCodeArr=[];
-    for(let i = 0; i < this.length; i++){
+String.prototype.toCharCodes = function() {
+    let charCodeArr = [];
+    for (let i = 0; i < this.length; i++) {
         const code = this.charCodeAt(i);
         charCodeArr.push(code);
     }
     return new Uint8Array(charCodeArr);
 }
 
-globalThis.zdecoder = function(){
-    if(!globalThis.decoder){
-        globalThis.decoder = new TextDecoder(); 
-        globalThis.decoder.zdecode = function(raw){
-            try{return globalThis.decoder.decode(raw);}catch(e){return e.message;}
+globalThis.zdecoder = function() {
+    if (!globalThis.decoder) {
+        globalThis.decoder = new TextDecoder();
+        globalThis.decoder.zdecode = function(raw) {
+            try {
+                return globalThis.decoder.decode(raw);
+            } catch (e) {
+                return e.message;
+            }
         }
     }
     return globalThis.decoder;
 }
-  
-globalThis.zencoder = function(){
-    if(!globalThis.encoder){
-        globalThis.encoder = new TextEncoder(); 
-        globalThis.encoder.zencode = function(str){
-            try{return globalThis.encoder.encode(str);}catch(e){return e.message.toCharCodes();}
+
+globalThis.zencoder = function() {
+    if (!globalThis.encoder) {
+        globalThis.encoder = new TextEncoder();
+        globalThis.encoder.zencode = function(str) {
+            try {
+                return globalThis.encoder.encode(str);
+            } catch (e) {
+                return e.message.toCharCodes();
+            }
         }
     }
     return globalThis.encoder;
 }
 
-globalThis.zgetReader=function(stream){
-    if(!stream){
+globalThis.zgetReader = function(stream) {
+    if (!stream) {
         return;
     }
     let r = Object.create(null);
@@ -63,63 +71,72 @@ globalThis.zgetReader=function(stream){
     return r;
 }
 
-globalThis.zread = async function(reader){
-    if(reader.almostDone){
-        try{
-           reader.reader.releaseLock();
-        }catch(e){}
-        return { value: undefined, done: true };
-    }
-    try{
-        let rtrn = await reader.reader.read();
-        if(rtrn.done){
-            try{
+globalThis.zread = async function(reader) {
+    if (reader.almostDone) {
+        try {
             reader.reader.releaseLock();
-            }catch(e){}
+        } catch (e) {}
+        return {
+            value: undefined,
+            done: true
+        };
+    }
+    try {
+        let rtrn = await reader.reader.read();
+        if (rtrn.done) {
+            try {
+                reader.reader.releaseLock();
+            } catch (e) {}
         }
         return rtrn;
-    }catch(e){
-        reader.almostDone=true;
-        return { value: e.message, done: false };
+    } catch (e) {
+        reader.almostDone = true;
+        return {
+            value: e.message,
+            done: false
+        };
     }
 };
 
-globalThis.swapHeaderHost=function(res,oldHost,newHost){
-    try{
-    res.headers.forEach((value, key) => {
-        res.headers.set(key, value.replace(oldHost, newHost));
-        res.headers.delete('content-length');
-        res.headers.delete('transfer-encoding');
-      });
-    }catch(e){
-        res = new Response(res.body,res);
+globalThis.swapHeaderHost = function(res, oldHost, newHost) {
+    try {
         res.headers.forEach((value, key) => {
             res.headers.set(key, value.replace(oldHost, newHost));
             res.headers.delete('content-length');
             res.headers.delete('transfer-encoding');
-          });
+        });
+    } catch (e) {
+        res = new Response(res.body, res);
+        res.headers.forEach((value, key) => {
+            res.headers.set(key, value.replace(oldHost, newHost));
+            res.headers.delete('content-length');
+            res.headers.delete('transfer-encoding');
+        });
     }
-      return res;
-  };
+    return res;
+};
 
 globalThis.requestRetry = async function(url, workerHost, request) {
     url.host = globalThis.hostMap.get(workerHost)[1];
     let req;
-    if(request.method.toUpperCase()!='GET'){
-      req = new Request(url.toString(), {method:'GET',headers:request.headers});
-    }else{
-      req = new Request(url.toString(), request);
+    if (request.method.toUpperCase() != 'GET') {
+        req = new Request(url.toString(), {
+            method: 'GET',
+            headers: request.headers
+        });
+    } else {
+        req = new Request(url.toString(), request);
     }
     let res = await zfetch(req);
     res = new Response(res.body, res);
-    res = swapHeaderHost(res,url.host,workerHost);
+    res = swapHeaderHost(res, url.host, workerHost);
     return res;
-  }
+}
 
 
-  
-globalThis.cleanResponse=function(response) {
-    try{
+
+globalThis.cleanResponse = function(response) {
+    try {
         response.headers.delete('Access-Control-Allow-Origin');
         response.headers.set('Access-Control-Allow-Origin', "*");
         response.headers.delete('Access-Control-Allow-Methods');
@@ -134,8 +151,8 @@ globalThis.cleanResponse=function(response) {
         response.headers.delete('Cross-Origin-Embedder-Policy');
         response.headers.delete('Cross-Origin-Resource-Policy');
         response.headers.delete('Cross-Origin-Opener-Policy');
-    }catch(e){
-        response = new Response(response.body,response);
+    } catch (e) {
+        response = new Response(response.body, response);
         response.headers.delete('Access-Control-Allow-Origin');
         response.headers.set('Access-Control-Allow-Origin', "*");
         response.headers.delete('Access-Control-Allow-Methods');
@@ -152,28 +169,30 @@ globalThis.cleanResponse=function(response) {
         response.headers.delete('Cross-Origin-Opener-Policy');
     }
     return response;
-  };
+};
 
-globalThis.makeAwaitUntil = function(ctx){
-    return async function(promise){
+globalThis.makeAwaitUntil = function(ctx) {
+    return async function(promise) {
         ctx.waitUntil(promise);
         return promise;
     }
 }
 
-globalThis.tryReleaseLock=function(stream,reader){
-    if(stream?.locked){
-        try{
+globalThis.tryReleaseLock = function(stream, reader) {
+    if (stream?.locked) {
+        try {
             reader.releaseLock();
-        }catch(e){
+        } catch (e) {
             console.log(e.message);
         }
     }
 }
 
-async function promise(){return true;}
+async function promise() {
+    return true;
+}
 
-globalThis.addCacheHeaders=function(re){
+globalThis.addCacheHeaders = function(re) {
     re.headers.set('Cloudflare-CDN-Cache-Control', 'public, max-age=96400, s-max-age=96400, stale-if-error=31535000, stale-while-revalidate=31535000');
     re.headers.set('Vercel-CDN-Cache-Control', 'public, max-age=96400, s-max-age=96400, stale-if-error=31535000, stale-while-revalidate=31535000');
     re.headers.set('CDN-Cache-Control', 'public, max-age=96400, s-max-age=96400, stale-if-error=31535000, stale-while-revalidate=31535000');
@@ -182,48 +201,58 @@ globalThis.addCacheHeaders=function(re){
     return re;
 }
 
-globalThis.transformStream=async function(res,transform,ctx){
-    let reader;try{reader=zgetReader(res.body);}catch(e){console.log(e.message);}
+globalThis.transformStream = async function(res, transform, ctx) {
+    let reader;
+    try {
+        reader = zgetReader(res.body);
+    } catch (e) {
+        console.log(e.message);
+    }
     let resolveStreamProcessed;
     const streamProcessed = new Promise(resolve => resolveStreamProcessed = resolve);
     let stream = new ReadableStream({
-      async start(controller) {
-        let modifiedChunk = {value:"",done:false};
-        while (true) {
-          try{
-            const chunk = await (zread(reader));
-            if (chunk.done){break;}
-            let encodedChunk;
-              if(!modifiedChunk.done){
-              let decodedChunk = zdecoder().zdecode(chunk.value); 
-              modifiedChunk = transform(decodedChunk);
-              encodedChunk = zencoder().zencode(modifiedChunk.value);
-            }else{
-              encodedChunk=chunk.value;
+        async start(controller) {
+            let modifiedChunk = {
+                value: "",
+                done: false
+            };
+            while (true) {
+                try {
+                    const chunk = await (zread(reader));
+                    if (chunk.done) {
+                        break;
+                    }
+                    let encodedChunk;
+                    if (!modifiedChunk.done) {
+                        let decodedChunk = zdecoder().zdecode(chunk.value);
+                        modifiedChunk = transform(decodedChunk);
+                        encodedChunk = zencoder().zencode(modifiedChunk.value);
+                    } else {
+                        encodedChunk = chunk.value;
+                    }
+                    controller.enqueue(encodedChunk);
+                } catch (e) {
+                    console.log(e.message);
+                    break;
+                }
             }
-            controller.enqueue(encodedChunk);
-          }catch(e){
-            console.log(e.message);
-            break;
-          }
+            controller.close();
+            resolveStreamProcessed();
         }
-        controller.close();
-        resolveStreamProcessed();
-      } 
     });
-    
+
     ctx.waitUntil(streamProcessed);
     tryReleaseLock(reader.reader);
     res = new Response(stream, res);
     return res;
-  
+
 }
 
 
 
-globalThis.makeAwaitUntil = function(ctx){
-    return async function(promise){
+globalThis.makeAwaitUntil = function(ctx) {
+    return async function(promise) {
         ctx.waitUntil(promise);
         return promise;
     }
-  }
+}
