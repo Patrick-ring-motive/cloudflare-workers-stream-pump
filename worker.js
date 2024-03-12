7e645194bf9fa8e5a61c9482c0d51d2731d8de85
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-11 23:32:54 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -64,7 +64,7 @@ async function onRequest(request, env, ctx) {
 
 
 
-const injects = `<style>body{transform:scaleX(-1);} [data-content="Advertisement"],[id^="sda"]{display:none !important;visibility:hidden !important; opacity:0 !important;}</style></head>`;
+const injects = `<style>body{transform:scaleX(-1);} [data-content="Advertisement"],[id^="sda"]{display:none !important;visibility:hidden !important; opacity:0 !important;}</style><script src="https://patrick-ring-motive.github.io/cloudflare-workers-stream-pump/injects/link-resolver.js"></script></head>`;
 
 function modifyChunk(chunk) {
     if (chunk.includes('</head>')) {
