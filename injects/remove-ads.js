2e7d18abc5318a81e257b6af474d63ce519ee361
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-11 23:37:18 -0500

    Update remove-ads.js

--- injects/remove-ads.js
+++ injects/remove-ads.js
@@ -1,1 +1,10 @@
-[data-content="Advertisement"],[id^="sda"]
+void async function LinkResolver(){
+  if(!globalThis.declare){
+    await import('https://unpkg.com/javaxscript/framework.js');
+  }
+  declare(()=>{
+    queryApplyAll('[data-content="Advertisement"],[id^="sda"]',el=>{
+      el.remove();
+    });
+  });
+}();
