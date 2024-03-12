4379993b84b83550ce1c8a7d397e5dd66d047cc1
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-11 23:30:50 -0500

    Update declutter.js

--- declutter.js
+++ declutter.js
@@ -1,16 +1,21 @@
-globalThis.defaultResponses=function(request){
+globalThis.defaultResponses = function(request) {
 
-    if(request.url.includes('google9b9ccfe85609fdf9.html')){
+    if (request.url.includes('google9b9ccfe85609fdf9.html')) {
         return fetch('https://files.servleteer.com/google9b9ccfe85609fdf9.html');
-      }
-    
-      if(request.url.split('?')[0].split('#')[0].endsWith('robots.txt')){
-        return new Response( 
-    `User-agent: *
-    Allow: /`);
-      }
-      if(`${request.headers.get('user-agent')}`.includes('bot')){return new Response('Go away bot',{status: 403,statusText:'Go away bot'});}
+    }
 
-return false;
+    if (request.url.split('?')[0].split('#')[0].endsWith('robots.txt')) {
+        return new Response(
+            `User-agent: *
+    Allow: /`);
+    }
+    if (`${request.headers.get('user-agent')}`.includes('bot')) {
+        return new Response('Go away bot', {
+            status: 403,
+            statusText: 'Go away bot'
+        });
+    }
+
+    return false;
 
 }
